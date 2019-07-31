e7ccae265d1327b032323c9d188fcad5a4d4d80c
Alexandre Queiroz <alexandre@beestart.com.br>
2019-07-31 12:45:22 -0300

    Alterando url produção

--- src/Api/getUrl.js
+++ src/Api/getUrl.js
@@ -4,6 +4,6 @@ function getURL() {
   if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
     return "http://localhost:5001/node-iextrading-5e2d2/us-central1/app";
   } else {
-    return "https://us-central1-mininglab-db-auth-storage.cloudfunctions.net/app";
+    return "https://us-central1-node-iextrading-5e2d2.cloudfunctions.net/app";
   }
 }
